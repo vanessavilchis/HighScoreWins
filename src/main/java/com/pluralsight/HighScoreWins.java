35f02c8d7feca39234b872ed862f5deb239bc401
vanessavilchis <vanessavilchis.info@gmail.com>
2025-09-30 11:08:11 -0400

    second commit

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class HighScoreWins {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        String[] game = input.split("\\|");
-        String [] teams = game[0].split(":");;
+        System.out.println("please enter game score: ");
+        String input = scan.nextLine().trim();
+
+        String[] parts = input.split("\\|");
+        String [] teams = parts[0].split("\\:");;
+        String [] scores = parts[1].split("\\:");;
+
         String homeTeam = teams[0];
         String visitorTeam = teams[1];
 
-        String[] scores = game[1].split(":");
-        int homeTeamScore = teams [0];
-        String []
+        int homeTeamScore = Integer.parseInt(scores[0]);
+        int visitorTeamScore = Integer.parseInt(scores[1]);
+
+        if (homeTeamScore > visitorTeamScore
+
     }
 }
